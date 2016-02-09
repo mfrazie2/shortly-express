9e80207d8765db492b990c50c33b10c40c6020fa
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-09 14:28:52 -0600

    Restricted personal access to the website while figuring out sessions. Needs improvement.

--- shortly.js
+++ shortly.js
@@ -3,6 +3,7 @@ var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var util2 = require('util');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -30,25 +31,41 @@ app.use(session({
   secret: 'ILLINI'
 }));
 
-app.get('/', restrict,
+var sess;
+
+app.get('/',
 function(req, res) {
-  res.render('index');
+  restrict(req, res, function(res) {
+    res.render('index');
+  });
+  // sess = req.session;
 });
 
-app.get('/create', restrict,
+app.get('/create',
 function(req, res) {
-  res.render('index');
+  restrict(req, res, function(res) {
+    res.render('create');
+  });
+  // sess = req.session;
+  // res.render('create');
 });
 
-app.get('/links', restrict,
+app.get('/links',
 function(req, res) {
-  Links.reset().fetch().then(function(links) {
-    res.send(200, links.models);
+  restrict(req, res, function(res) {
+    sess = req.session;
+    console.log('session ' + req.session);
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
   });
 });
 
 app.post('/links',
 function(req, res) {
+  // sess = req.session;
+
+
   var uri = req.body.url;
 
   if (!util.isValidUrl(uri)) {
@@ -84,9 +101,18 @@ function(req, res) {
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
-function restrict(req, res, next) {
-  if(req.session.user) {
-    next();
+function restrict(req, res, callback) {
+  // sess.username = req.body.username;
+  // sess.whatever = 'what?';
+  // console.log('Check restrict');
+  // console.log('session ' + req.session.id);
+  // console.log('session ' + req);
+  // console.log(util2.inspect(sess, false, null));
+  // console.log('req.body ' + util2.inspect(req.body, false, null));
+  // console.log(sess.username);
+  // console.log(req.body.username);
+  if(sess && sess.username && sess.username === req.body.username) {
+    callback(res);
   } else {
     req.session.error = "Access Denied!";
     res.redirect("/login");
@@ -94,16 +120,20 @@ function restrict(req, res, next) {
 }
 
 app.get("/login", function(req, res) {
+  // sess = req.session;
+
   res.render('login');
 });
 
 app.get("/signup", function(req, res) {
+  // sess = req.session;
   res.render('signup');
 });
 
 app.post("/signup", function(req, res) {
+  // sess = req.session;
   var username = req.body.username;
-  var password = req.body.username;
+  var password = req.body.password;
 
   new User({ username: username }).fetch().then(function(found) {
     if (found) {
@@ -117,33 +147,34 @@ app.post("/signup", function(req, res) {
 
       user.save().then(function(newUser) {
         Users.add(newUser);
-        res.render('login');
+        res.redirect('/login');
       });
     }
   });
 });
 
 app.post("/login", function(req, res) {
+  sess = req.session;
   // get username/password from page
   var username = req.body.username;
-  var password = req.body.username;
+  var password = req.body.password;
+  console.log('--->' + util2.inspect(sess, false, null));
   // username exist?
     // check password against hash
   // check agains db
   new User({ username: username }).fetch().then(function(found) {
     if (found) {
       if (found.comparePassword(password)) {
-        res.render('index');
+        res.redirect('/');
+        sess.username = username;
       }
-
-
     } else {
-      res.render('login');
+      res.redirect('/login');
     }
   });
-
 });
 
+//app.get();
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
 // assume the route is a short code and try and handle it here.
@@ -151,6 +182,8 @@ app.post("/login", function(req, res) {
 /************************************************************/
 
 app.get('/*', function(req, res) {
+  // sess = req.session;
+
   new Link({ code: req.params[0] }).fetch().then(function(link) {
     if (!link) {
       res.redirect('/');
